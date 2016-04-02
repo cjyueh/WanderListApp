dc4ff3b70143764ca4be3806d7783666f0435e0e
cjyueh <cjyueh@gmail.com>
2016-04-02 16:02:28 -0700

    renames DestinationsCtrl to MainCtrl because controls homepage view and route

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var app = angular.module('WanderList', ['ui.router']);
 
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
   $stateProvider
+    //get homepage
     .state('home', {
       url: '/home',
       templateUrl: '/home.html',
-      controller: 'DestinationsCtrl'
+      controller: 'MainCtrl'
     });
   $urlRouterProvider.otherwise('home');
 }]);
@@ -17,7 +18,7 @@ app.factory('destinations', [function() {
   return o;
 }]);
 
-app.controller('DestinationsCtrl', ['$scope', 'destinations', function($scope, destinations) {
+app.controller('MainCtrl', ['$scope', 'destinations', function($scope, destinations) {
   $scope.test = 'Hello world!';
   $scope.destinations = destinations.destinations;
   $scope.addDestination = function() {
