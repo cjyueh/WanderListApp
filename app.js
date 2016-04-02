b80125e35cc5bf78a1449daa41c8dd2b7fe1bc8b
cjyueh <cjyueh@gmail.com>
2016-04-02 15:17:36 -0700

    creates DestinationsCtrl to get list of destinations and add one, sets up index view page for all destinations and form to add a destination

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ var app = angular.module('WanderList', []);
 app.controller('DestinationsCtrl', ['$scope', function($scope) {
   $scope.test = 'Hello world!';
   $scope.destinations = [
-    'Vernazza, Italy'
+    {name: 'place1'},
+    {name: 'place2'},
+    {name: 'place3'}
   ];
+  $scope.addDestination = function() {
+    if(!$scope.name || $scope.name === '') { return; }
+    $scope.destinations.push({name: $scope.name});
+    $scope.name = '';
+  };
 }]);
