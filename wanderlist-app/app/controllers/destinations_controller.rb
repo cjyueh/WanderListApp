004ce0b1e8a4d84e73a95aaeb792965bbb662875
cjyueh <cjyueh@gmail.com>
2016-04-04 13:09:19 -0700

    sets up DestinationsController for index, create, and show

--- wanderlist-app/app/controllers/destinations_controller.rb
+++ wanderlist-app/app/controllers/destinations_controller.rb
@@ -3,7 +3,17 @@ class DestinationsController < ApplicationController
 	def index
 		respond_with Destination.all
 	end
-
+	# POST to create a new destination using params from form input
 	def create
-		respond_with Destination.create(Destination_params)
+		respond_with Destination.create(destination_params)
+	end
+	# GET a destination using the id from params
+	def show
+		respond_with Destination.find(params[:id])
+	end
+
+	private
+	def destination_params
+		params.require(:destination).permit(:name)
+	end
 end
