87ed57f82d3e75df4c3d9780c80948662285ebae
cjyueh <cjyueh@gmail.com>
2016-04-04 11:06:38 -0700

    creates routes for destinations, itineraries, activities, users, and sessions

--- wanderlist-app/config/routes.rb
+++ wanderlist-app/config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-	root to: 'application#angular'
+	root to: "application#angular"
+
+	resources :destinations, only: [:create, :index, :show] do
+		resources :itineraries do
+			resources :activities
+		end
+	end
+
+	resources :users
+
+	get "/log-in", to: "sessions#new"
+	post "/sessions", to: "sessions#create"
+	delete "/log-out", to: "sessions#destroy", as: "sign_out"
 end
