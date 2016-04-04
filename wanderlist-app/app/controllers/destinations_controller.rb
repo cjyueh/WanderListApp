20cbcb3eb7e809c936e70d0e96561eb3c0840ac9
cjyueh <cjyueh@gmail.com>
2016-04-04 11:46:01 -0700

    starts controllers and changes db from sqlite to postgresql

--- wanderlist-app/app/controllers/destinations_controller.rb
+++ wanderlist-app/app/controllers/destinations_controller.rb
@@ -1,2 +1,9 @@
 class DestinationsController < ApplicationController
+	# GET all the destinations as json
+	def index
+		respond_with Destination.all
+	end
+
+	def create
+		respond_with Destination.create(Destination_params)
 end
