52b80eb930312206ceb027f29982bb61f6935771
cjyueh <cjyueh@gmail.com>
2016-04-05 21:22:42 -0700

    adds login and register functions to AuthCtrl

--- wanderlist-app/app/assets/javascripts/auth/authCtrl.js
+++ wanderlist-app/app/assets/javascripts/auth/authCtrl.js
@@ -1,5 +1,17 @@
 var app = angular.module('WanderList');
 
 app.controller('AuthCtrl', ['$scope', '$state', 'Auth', function($scope, $state, Auth) {
-  
+  //when user is logged in, redirect to 'home'
+  $scope.login = function() {
+    Auth.login($scope.user).then(function() {
+      $state.go('home');
+    });
+  };
+
+  //when user is signed up, redirect to 'home'
+  $scope.register = function() {
+    Auth.register($scope.user).then(function() {
+      $state.go('home');
+    });
+  };
 }]);
