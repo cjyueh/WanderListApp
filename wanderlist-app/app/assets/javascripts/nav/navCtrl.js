8a26255e8d38f75a40f57d0213b4e2a7a863399b
cjyueh <cjyueh@gmail.com>
2016-04-05 21:13:28 -0700

    adds event listeners to NavCtrl

--- wanderlist-app/app/assets/javascripts/nav/navCtrl.js
+++ wanderlist-app/app/assets/javascripts/nav/navCtrl.js
@@ -1,5 +1,26 @@
 var app = angular.module('WanderList');
 
 app.controller('NavCtrl', ['$scope', 'Auth', function($scope, Auth) {
-  
+  $scope.loggedIn = Auth.isAuthenticated;
+  $scope.logout = Auth.logout;
+
+  //access current user to set user
+  Auth.currentUser().then(function(user) {
+    $scope.user = user;
+  });
+
+  //event listener for when new user signs up and set user
+  $scope.$on('devise:new-registration', function(e, user) {
+    $scope.user = user;
+  });
+
+  //event listener for when user logs in & set user
+  $scope.$on('devise:login', function(e, user) {
+    $scope.user = user;
+  });
+
+  //event listener for when user logs out & clear out user
+  $scope.$on('devise:logout', function(e, user) {
+    $scope.user = {};
+  });
 }]);
