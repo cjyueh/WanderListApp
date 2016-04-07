66e1ce5266633c79dd041c853251f31ad8fc4e3a
cjyueh <cjyueh@gmail.com>
2016-04-07 10:22:27 -0700

    adds destinations to seeds file

--- wanderlist-app/db/seeds.rb
+++ wanderlist-app/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# seeding db with list of destinations
+Destination.create({name: "Varenna, Italy"})
+Destination.create({name: "Ubud, Bali, Indonesia"})
+Destination.create({name: "Singapore"})
+Destination.create({name: "Kuala Lumpur, Malaysia"})
+Destination.create({name: "Panang, Malaysia"})
+Destination.create({name: "Malacca, Malaysia"})
+Destination.create({name: "Hanoi, Vietnam"})
+Destination.create({name: "Ho Chi Minh City, Vietnam"})
+Destination.create({name: "Hong Kong"})
+Destination.create({name: "Taipei, Taiwan"})
+Destination.create({name: "Beijing, China"})
+Destination.create({name: "Avignon, France"})
+Destination.create({name: "Florence, Italy"})
+Destination.create({name: "Sorrento, Italy"})
+Destination.create({name: "Oslo, Norway"})
+Destination.create({name: "Roatan, Honduras"})
