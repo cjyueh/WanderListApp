5538cf002abd81d2fb9fa31e7ec327f8a7f67e50
cjyueh <cjyueh@gmail.com>
2016-04-07 15:05:24 -0700

    gets user show page to work

--- wanderlist-app/app/assets/javascripts/users/usersCtrl.js
+++ wanderlist-app/app/assets/javascripts/users/usersCtrl.js
@@ -1,13 +1,8 @@
 var app = angular.module('WanderList');
 
 app.controller('UsersCtrl', ['$scope', 'Auth', function($scope, Auth) {
-  $scope.test = "hi";
-  // //get a particular user
-  // $scope.user = user;
-
-  //access current user to set user
+  //access current user
   Auth.currentUser().then(function(user) {
     $scope.user = user;
   });
-  
 }]);
